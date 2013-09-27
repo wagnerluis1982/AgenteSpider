099168461aafa3e7524c5f8f2734b904f7fc9341
Wagner Macedo <wagnerluis1982@gmail.com>
2013-09-27 16:46:25 -0300

    Changes and more changes

--- Spider.java
+++ Spider.java
@@ -4,22 +4,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Spider {
 	protected static final Pattern HREF_REGEX = Pattern.compile(
-			"href=['\"]?([^'\" <>]*)['\"]?", Pattern.CASE_INSENSITIVE);
+			"href=\"([^'\" <>]*)\"", Pattern.CASE_INSENSITIVE);
+	protected static final Pattern HOST_REGEX = Pattern.compile(
+			"^http://([a-z.]+)");
 
 	protected final String baseAddress;
 	protected final String baseHost;
+	protected final Map<String, Socket> openSockets;
 
+	// Construtor
 	public Spider(String baseAddress) {
 		if (baseAddress == null)
 			throw new NullPointerException();
@@ -31,14 +38,7 @@ public class Spider {
 
 		this.baseAddress = baseAddress;
 		this.baseHost = getHost(baseAddress);
-	}
-
-	protected String getHost(String address) {
-		return address.substring(7, address.indexOf("/", 7));
-	}
-
-	protected String getPath(String address) {
-		return address.substring(address.indexOf("/", 7));
+		this.openSockets = new ConcurrentHashMap<>();
 	}
 
 	protected boolean isValidArg(final String address) {
@@ -48,51 +48,10 @@ public class Spider {
 		return false;
 	}
 
-	/**
-	 * Obtém todos os links em uma página HTML, passada como argumento através
-	 * de um {@link InputStream}.
-	 *
-	 * @param buffer Página HTML
-	 * @return Lista de links encontrados
-	 * @throws IOException se ocorrer um erro de E/S
-	 */
-	protected List<Link> findLinks(final BufferedReader buffer) {
-		final List<Link> foundLinks = new ArrayList<>();
-
-		String line;
-		try {
-			for (int no = 1; (line=buffer.readLine()) != null; no++) {
-				final Matcher matcher = HREF_REGEX.matcher(line);
-				while (matcher.find()) {
-					String linkTo = this.absoluteLink(matcher.group(1));
-					if (linkTo != null)
-						foundLinks.add(new Link(this.baseAddress, linkTo, no));
-				}
-			}
-			buffer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return foundLinks;
-	}
-
-	protected String absoluteLink(String link) {
-		try {
-			// Link já é absoluto (somente http)
-			if (link.startsWith("http:"))
-				return normalizedLink(link);
-
-			// Link relativo à raiz deve se tornar absoluto
-			if (link.startsWith("/"))
-				return "http://" + this.baseHost + normalizedLink(link);
-
-			// Links relativos
-			return normalizedLink(this.baseAddress + link);
-		} catch (NormalizationException e) {}
-
-		// Outros protocolos e links mal formados não são suportados
-		return null;
+	protected String getHost(String address) {
+		Matcher matcher = HOST_REGEX.matcher(address);
+		matcher.find();
+		return matcher.group(1);
 	}
 
 	/**
@@ -102,7 +61,11 @@ public class Spider {
 	 * @return String com o link normalizado
 	 */
 	protected String normalizedLink(String link) throws NormalizationException {
-		List<String> pieces = new LinkedList<>(Arrays.asList(link.split("/+")));
+		String[] split = link.split("/+");
+		if (split.length == 0)
+			return link;
+
+		List<String> pieces = new LinkedList<>(Arrays.asList(split));
 		for (int i = 0; i < pieces.size(); ) {
 			if (pieces.get(i).equals("."))
 				pieces.remove(i);
@@ -147,10 +110,66 @@ public class Spider {
 		return path.toString();
 	}
 
+	protected String absoluteLink(String link) {
+		try {
+			// Link já é absoluto (somente http)
+			if (link.startsWith("http:"))
+				return normalizedLink(link);
+
+			// Link relativo à raiz deve se tornar absoluto
+			if (link.startsWith("/"))
+				return "http://" + this.baseHost + normalizedLink(link);
+
+			// Links relativos
+			return normalizedLink(this.baseAddress + link);
+		} catch (NormalizationException e) {}
+
+		// Outros protocolos e links mal formados não são suportados
+		return null;
+	}
+
+	/**
+	 * Obtém todos os links em uma página HTML, passada como argumento através
+	 * de um {@link InputStream}.
+	 *
+	 * @param buffer Página HTML
+	 * @return Lista de links encontrados
+	 * @throws IOException se ocorrer um erro de E/S
+	 */
+	protected List<Link> findLinks(final BufferedReader buffer) {
+		final List<Link> foundLinks = new ArrayList<>();
+
+		String line;
+		try {
+			for (int no = 1; (line=buffer.readLine()) != null; no++) {
+				final Matcher matcher = HREF_REGEX.matcher(line);
+				while (matcher.find()) {
+					String linkTo = this.absoluteLink(matcher.group(1));
+					if (linkTo != null)
+						foundLinks.add(new Link(this.baseAddress, linkTo, no));
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return foundLinks;
+	}
+
+	protected Socket getHttpSocket(String host) throws UnknownHostException, IOException {
+		Socket sock = this.openSockets.get(host);
+		if (sock == null || sock.isClosed()) {
+			sock = new Socket(host, 80);
+			this.openSockets.put(host, sock);
+		}
+
+		return sock;
+	}
+
 	protected Page httpGet(String address) throws IOException {
 		// Conexão
 		String host = getHost(address);
-		Socket socket = new Socket(host, 80);
+		Socket socket = getHttpSocket(host);
 		OutputStream os = socket.getOutputStream();
 		InputStream  is = socket.getInputStream();
 
@@ -165,7 +184,6 @@ public class Spider {
 
 		// Só obtem conteúdo quando código for 200
 		if (code != 200) {
-			buffer.close();
 			return new Page(code, null);
 		}
 
@@ -173,6 +191,25 @@ public class Spider {
 		while (buffer.readLine().length() != 0);
 
 		return new Page(code, buffer);
+	}
+
+	protected int httpHead(String address) throws IOException {
+		// Conexão
+		String host = getHost(address);
+		Socket socket = getHttpSocket(host);
+		OutputStream os = socket.getOutputStream();
+		InputStream  is = socket.getInputStream();
+
+		// Requisição
+		String requisition = String.format("HEAD %s HTTP/1.1\r\n" +
+				"Host:%s\r\n\r\n", address, host);
+		os.write(requisition.getBytes());
+
+		// Código de resposta
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+		int code = Integer.parseInt(buffer.readLine().split(" ")[1]);
+
+		return code;
 	}
 
 	protected List<InvalidLink> invalidLinks(Link link) {
@@ -193,9 +230,9 @@ public class Spider {
 
 		for (Link found : findLinks(page.buffer)) {
 			try {
-				page = httpGet(found.linkTo);
-				if (page.code != 200)
-					invalids.add(new InvalidLink(found, page.code));
+				int code = httpHead(found.linkTo);
+				if (code != 200)
+					invalids.add(new InvalidLink(found, code));
 			} catch (IOException e) {
 				// Erro de DNS
 				invalids.add(new InvalidLink(found, 0));
@@ -231,7 +268,7 @@ public class Spider {
 
 		for (InvalidLink invalid : spider.invalidLinks()) {
 			Link link = invalid.link;
-			System.out.println(String.format("%s %d %s %d",
+			System.out.println(String.format("%s %03d %s %d",
 					link.linkTo, invalid.code, link.pageUrl, link.line));
 		}
 
@@ -269,6 +306,18 @@ class Page {
 	public Page(int code, BufferedReader buffer) {
 		this.code = code;
 		this.buffer = buffer;
+	}
+}
+
+class SpiderSocket {
+	final Socket sock;
+	final BufferedReader in;
+	final OutputStream out;
+
+	public SpiderSocket(Socket sock, BufferedReader in, OutputStream out) {
+		this.sock = sock;
+		this.in = in;
+		this.out = out;
 	}
 }
 
